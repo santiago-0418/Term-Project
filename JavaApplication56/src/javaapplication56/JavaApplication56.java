b13265cb3753396b0e52470d9726c882bbbeb08e
lince505 <linkys505@gmail.com>
2023-04-25 12:30:43 -0400

    Update JavaApplication56.java

--- JavaApplication56/src/javaapplication56/JavaApplication56.java
+++ JavaApplication56/src/javaapplication56/JavaApplication56.java
@@ -15,6 +15,7 @@ public class JavaApplication56 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("test");
         // TODO code application logic here
         Scanner user_input = new Scanner(System.in);
         int selection;
