52da4779ac06511a9d9e100d12c2362b85e4cdd9
lince505 <linkys505@gmail.com>
2023-05-15 10:14:47 -0400

    quick fix

--- Term Projet Sports/src/TermProject/SetUp.java
+++ Term Projet Sports/src/TermProject/SetUp.java
@@ -16,6 +16,7 @@ public class SetUp {
         new File(f_name).mkdirs();
         //Creation of Athletes folder
         new File((home+"/desktop/SportsPlatform/Athletes")).mkdirs();
+        new File((home+"/desktop/SportsPlatform/Athletes/test")).mkdirs();
         //Creating of Storage for login
         new File((home+"/desktop/SportsPlatform/LoginInformation")).mkdirs();
         BufferedWriter writer = new BufferedWriter(new FileWriter(home+"/desktop/SportsPlatform/LoginInformation/Admins.txt", false));
@@ -23,7 +24,12 @@ public class SetUp {
                         writer.close();
         BufferedWriter writer_2 = new BufferedWriter(new FileWriter(home+"/desktop/SportsPlatform/LoginInformation/Users.txt", false));
         writer_2.write("test:123:Swimming:test:noway:true,");
+        System.out.println("what is happenning");
         writer_2.close();
+        BufferedWriter writer_3 = new BufferedWriter(new FileWriter(home+"/desktop/SportsPlatform/Athletes/test/Swimming", false));
+        writer_3.write("0:0:0,");
+        writer_3.close();
+        
     }
     
 }
